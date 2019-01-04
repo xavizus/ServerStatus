eafb3f32f83937899ea320f7e3245a5b7e1192ee
Ljungros Stephan <stephan@xavizus.com>
2019-01-04 02:27:40 +0100

    Test

--- html/gitpull.php
+++ html/gitpull.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "This is a git pulls!";
+echo "This is a git pull!";
 if (preg_match("/192.168.2./i", $_SERVER['REMOTE_ADDR'])) {
 
     exec("cd /var/repo/git-site && /usr/bin/git pull --no-ff --no-edit https://UserName:Password@git.xavizus.com/Bonobo.Git.Server/ServerStatus.git Development 2>&1",$result);
