13ea7499cff4926852142184bbd2edc94cc96c63
Ljungros Stephan <stephan@xavizus.com>
2019-01-04 01:38:31 +0100

    added --no-edit

--- html/gitpull.php
+++ html/gitpull.php
@@ -3,7 +3,7 @@ echo "Git Pull!";
 $allow = array("192.168.2");
 if (in_array($_SERVER['REMOTE_ADDR'], $allow ) {
 
-    exec("cd /var/repo/git-site && git pull --no-ff https://UserName:Password@git.xavizus.com/Bonobo.Git.Server/ServerStatus.git Development",$result);
+    exec("cd /var/repo/git-site && git pull --no-ff --no-edit https://UserName:Password@git.xavizus.com/Bonobo.Git.Server/ServerStatus.git Development",$result);
     echo "<pre>";
     foreach($result as $line) {
         print($line . "\n");
